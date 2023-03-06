898a5e431579d9d97231ef6ddb705dee5763224f
Dendeey <davidsim1035@gmail.com>
2023-03-06 11:57:20 +0000

    Add list of routes, controllers, models and create models

---
+++ models/Media.php
@@ -0,0 +1,70 @@
+<?php
+
+class Media {
+    
+    // Attributes
+    
+    private ?int $id;
+    private string $url;
+    private string $caption;
+    
+
+    
+    // Constructor
+    
+    public function __construct(?int $id, string $url, string $caption)
+    {
+        $this->id = $id;
+        $this->url = $url;
+        $this->caption = $caption;
+        
+
+    }
+
+
+    // Getters
+
+    public function getId() : ?int
+    {
+        return $this->id;
+    }
+    
+    public function getUrl() : string
+    {
+        return $this->url;
+    }
+    
+    public function getCaption() : string
+    {
+        return $this->caption;
+    }
+    
+    
+
+    
+    
+    // Setters
+
+    public function setId(?int $id) : void
+    {
+        $this->id = $id;
+    }
+
+    public function setUrl(string $url) : void
+    {
+        $this->url = $url;
+    }
+    
+    public function setCaption(string $caption) : void
+    {
+        $this->caption = $caption;
+    }
+
+    
+
+    
+    // Functions
+
+}
+
+?>
