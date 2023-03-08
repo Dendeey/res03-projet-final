e9af56278a1c8794781a24d554d6975fd3c4db5d
Dendeey <davidsim1035@gmail.com>
2023-03-08 10:16:26 +0000

    Adding router

--- develop/index.php
+++ develop/index.php
@@ -2,28 +2,10 @@
 
 require "autoload.php";
 
-try {
+$router = new Router();
 
-    $router = new Router();
+$router->checkRoute();
 
-    if(isset($_GET['path']))
-    {
-        $request = "/".$_GET['path'];
-    }
-    else
-    {
-        $request = "/";
-    }
-    
-    $router->route($routes, $request);
-}
-catch(Exception $e)
-{
-    if($e->getCode() === 404)
-    {
-        
-    }
-}
 
 
 ?>
--- develop/services/Router.php
+++ develop/services/Router.php
@@ -1,66 +1,39 @@
 <?php
-/**
- * @author : Gaellan
- * @link : https://github.com/Gaellan 
- */
 
-class Router {
+// Requires //
+require '../controllers/PageController.php';
+require '../controllers/PlayerController.php';
 
-    private function parseRequest(string $request)
+
+class Router
+{
+
+    // Attributs //
+
+    private PageController $pageController;
+    private PlayerController $playerController;
+    
+
+    // Constructor //
+
+    public function __construct()
     {
-        $route = [];
-
-        $routeData = explode("/", $request); // we split the request using the / character
-
-        $route["path"] = "/".$routeData[1]; // the path is what is after the first /
-
-        if(count($routeData) > 2) // if we have more than one /
-        {
-            $route["parameter"] = $routeData[2]; // the parameter is after the second /
-        }
-        else
-        {
-            $route["parameter"] = null; // we don't have a parameter
-        }
-
-        return $route;
+        
+        $this->pageController = new PageController();
+        $this->playerController = new PlayerController();
+             
     }
 
-    public function route(array $routes, string $request)
+
+    // METHODES //
+
+    public function checkRoute(string $route) : void
     {
-        $requestData = $this->parseRequest($request); // we analyze the request and sort it
-
-        $routeFound = false;
-
-        foreach($routes as $route) // we go through the list of routes we built in the autoload
-        {
-            $controller = $route["controller"];
-            $method = $route["method"];
-            $parameter = $route["parameter"];
-
-            if($route["path"] === $requestData["path"]) // if the path exists
-            {
-                if($route["parameter"] && $requestData["parameter"] !== null) // if a parameter was needed and we have one
-                {
-                    $routeFound = true;
-
-                    $ctrl = new $controller();
-                    $ctrl->$method($requestData["parameter"]);
-                }
-                else if(!$route["parameter"] && $requestData["parameter"] === null) // or a parameter was not needed and we don't have one
-                {
-                    $routeFound = true;
-
-                    $ctrl = new $controller();
-                    $ctrl->$method($_POST);
-                }
-            }
-        }
-        if(!$routeFound) // anything else will throw an exception telling us the route does not exist
-        {
-            throw new Exception("Route not found", 404);
-        }
+        
+        if()
+        
     }
+
 }
 
-?>
+?>    
