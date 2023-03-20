9683aa7c6f28beeeef9e2e5e90b9c23a74bd9fa9
Dendeey <davidsim1035@gmail.com>
2023-03-20 16:25:09 +0000

    Adding media manager

--- usdomagne/managers/MediaManager.php
+++ usdomagne/managers/MediaManager.php
@@ -3,6 +3,23 @@
 class MediaManager extends AbstractManager
 {
     
+    //Créer une fonction qui ajoute une image dans la db
+    
+    public function insertImage(Media $image) : Media
+    {
+        $query = $this->db->prepare('INSERT INTO media (`id`, `url`, `caption`) VALUES(NULL, :url, :caption)');
+        $parameters = [
+            'url' => $image->getUrl(),
+            'caption' => $image->getCaption()
+            ];
+        $query->execute($parameters);
+        
+        $id = $this->db->lastInsertId();
+        $image->setId($id);
+        echo "Une image vient d'être ajoutée !";
+        return $image;
+    }
+    
     
 }
 
