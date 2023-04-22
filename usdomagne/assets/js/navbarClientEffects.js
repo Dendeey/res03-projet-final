04f91be73fa49b5a637a87de465614295f0681f1
David <davidsim1035@gmail.com>
2023-04-22 17:02:42 +0200

    Adding responsiv side menu

--- usdomagne/assets/js/navbarClientEffects.js
+++ usdomagne/assets/js/navbarClientEffects.js
@@ -5,8 +5,7 @@ function hideAndShowMenuOnScroll(){
     
     let prevScrollpos = window.pageYOffset;
 
-    window.onscroll = function() 
-    {
+    window.onscroll = function() {
         let currentScrollPos = window.pageYOffset;
         if (prevScrollpos > currentScrollPos) 
         {
@@ -22,14 +21,22 @@ function hideAndShowMenuOnScroll(){
 }
 
 /**
- *  Function qui permettra d'ouvrir et fermer le menu aside en cliquant dessus
+ *  Function qui permettra d'ouvrir et fermer le menu aside en cliquant sur le boutton
  */
 function openAndCloseAsideMenu(){
-
-    let sideMenuBtn = document.getElementById('sideMenu-btn');
+    let sideMenuBtn = document.querySelector('.sideMenu-btn');
+    let asideMenu = document.querySelector('.sideMenu');
+    let sideMenuMobileBtn = document.querySelector('.sideMenuMobile-btn');
+    let asideMenuMobile = document.querySelector('.sideMenuMobile');
 
     sideMenuBtn.addEventListener('click', function(){
-        
+        sideMenuBtn.classList.toggle('active');
+        asideMenu.classList.toggle('active');
+    })
+
+    sideMenuMobileBtn.addEventListener('click', function(){
+        sideMenuMobileBtn.classList.toggle('active');
+        asideMenuMobile.classList.toggle('active');
     })
     
 }
