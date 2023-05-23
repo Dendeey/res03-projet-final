1f19a83f6173437fb9156db19bf08afc2846d718
Dendeey <davidsim1035@gmail.com>
2023-05-23 16:40:00 +0000

    Adding carousel git status cd

---
+++ usdomagne/assets/js/carousel.js
@@ -0,0 +1,29 @@
+// Slider pour la section "partenaires" sur la page d'accueil
+function partenairesSlider() {
+    
+    // Ajouter un évènement au click sur les bouttons précédent et suivant 
+    let previousBtn = document.querySelector('.slider_btn_prev');
+    previousBtn.addEventListener('click', function() {
+        
+        // Récupérer la largeur de l'élément pour ensuite l'ajouter au moment du click
+        let figureWitdh = document.querySelector('.slider_content_item').offsetWidth;
+        document.querySelector('.slider_content').scrollLeft -= figureWitdh;
+    });
+
+    let nextBtn = document.querySelector('.slider_btn_next');
+    nextBtn.addEventListener('click', function() {
+        let figureWitdh = document.querySelector('.slider_content_item').offsetWidth;
+        document.querySelector('.slider_content').scrollLeft += figureWitdh;
+    });
+
+}
+
+// Slider pour la section "homepage-carousel" sur la page d'accueil
+function mainSlider(){
+    
+    
+    
+}
+
+export { partenairesSlider };
+export { mainSlider };
--- usdomagne/assets/js/index.js
+++ usdomagne/assets/js/index.js
@@ -1,5 +1,7 @@
 import { hideAndShowMenuOnScroll } from './navbarClientEffects.js';
 import { openAndCloseAsideMenu } from './navbarClientEffects.js';
+import { partenairesSlider } from './carousel.js';
+import { mainSlider } from './carousel.js';
 // import { validatorContact } from './validatorContact.js';
 
 
@@ -7,6 +9,8 @@ window.addEventListener('DOMContentLoaded', function() {
 
     hideAndShowMenuOnScroll();
     openAndCloseAsideMenu();
+    partenairesSlider();
+    mainSlider();
     // validatorContact();
 
 });
