1ee0f3585cbb14f6f283ff43d1cd03662fdf24f1
David <davidsim1035@gmail.com>
2023-07-25 16:33:45 +0200

    Main slider homepage

--- usdomagne/assets/js/carousel.js
+++ usdomagne/assets/js/carousel.js
@@ -1,31 +1,31 @@
 // Slider pour la section "partenaires" sur la page d'accueil
 function partenairesSlider() {
-    
+
     // Ajouter un évènement au click sur les bouttons précédent et suivant 
     let previousBtn = document.querySelector('.slider_btn_prev');
     if (previousBtn) {
-        previousBtn.addEventListener('click', function() {
-    
+        previousBtn.addEventListener('click', function () {
+
             // Récupérer la largeur de l'élément pour ensuite l'ajouter au moment du click
             let figureWitdh = document.querySelector('.slider_content_item').offsetWidth;
             document.querySelector('.slider_content').scrollLeft -= figureWitdh;
         });
     }
-    
+
 
     let nextBtn = document.querySelector('.slider_btn_next');
     if (nextBtn) {
-        nextBtn.addEventListener('click', function() {
+        nextBtn.addEventListener('click', function () {
             let figureWitdh = document.querySelector('.slider_content_item').offsetWidth;
             document.querySelector('.slider_content').scrollLeft += figureWitdh;
         });
     }
-    
+
 
 }
 
 // Slider pour la section "homepage-carousel" sur la page d'accueil
-function mainSlider(){
+function mainSlider() {
 
     // Récupération des éléments du slider
     const slider = document.querySelector(".mainSlider");
@@ -36,18 +36,17 @@ function mainSlider(){
 
     // Fonction pour afficher le slide suivant
     function showNextSlide() {
-
         sliderItems[currentIndex].classList.remove("active");
         currentIndex = (currentIndex + 1) % sliderItems.length;
         sliderItems[currentIndex].classList.add("active");
-    }
+      }
 
     // Fonction pour afficher le slide précédent
     function showPrevSlide() {
         sliderItems[currentIndex].classList.remove("active");
         currentIndex = (currentIndex - 1 + sliderItems.length) % sliderItems.length;
         sliderItems[currentIndex].classList.add("active");
-    }
+      }
 
     // Écouteurs d'événements pour les boutons de navigation
     sliderNavPrev.addEventListener("click", showPrevSlide);
