701e2581f85c08fb8f9ac778a8e2d9585a89790a
David <davidsim1035@gmail.com>
2023-08-03 18:39:53 +0200

    Sponsors slider homepage

--- usdomagne/assets/js/carousel.js
+++ usdomagne/assets/js/carousel.js
@@ -1,26 +1,26 @@
 // Slider pour la section "partenaires" sur la page d'accueil
 function partenairesSlider() {
 
-    // Ajouter un évènement au click sur les bouttons précédent et suivant 
-    let previousBtn = document.querySelector('.slider_btn_prev');
-    if (previousBtn) {
-        previousBtn.addEventListener('click', function () {
+    const carouselTrack = document.querySelector('.slider_content');
+    const images = carouselTrack.querySelectorAll('.slider_content_item');
+    const imageWidth = images[0].getBoundingClientRect().width;
+    const numVisibleImages = 3;
+    let currentIndex = 0;
 
-            // Récupérer la largeur de l'élément pour ensuite l'ajouter au moment du click
-            let figureWitdh = document.querySelector('.slider_content_item').offsetWidth;
-            document.querySelector('.slider_content').scrollLeft -= figureWitdh;
-        });
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % images.length;
+        moveCarousel();
     }
 
-
-    let nextBtn = document.querySelector('.slider_btn_next');
-    if (nextBtn) {
-        nextBtn.addEventListener('click', function () {
-            let figureWitdh = document.querySelector('.slider_content_item').offsetWidth;
-            document.querySelector('.slider_content').scrollLeft += figureWitdh;
-        });
+    function moveCarousel() {
+        carouselTrack.style.transform = `translateX(-${currentIndex * imageWidth}px)`;
     }
 
+    function initCarousel() {
+        setInterval(nextSlide, 3000); // Changer d'image toutes les 3 secondes (ajustez selon vos préférences)
+    }
+
+    window.addEventListener('load', initCarousel);
 
 }
 
@@ -39,14 +39,14 @@ function mainSlider() {
         sliderItems[currentIndex].classList.remove("active");
         currentIndex = (currentIndex + 1) % sliderItems.length;
         sliderItems[currentIndex].classList.add("active");
-      }
+    }
 
     // Fonction pour afficher le slide précédent
     function showPrevSlide() {
         sliderItems[currentIndex].classList.remove("active");
         currentIndex = (currentIndex - 1 + sliderItems.length) % sliderItems.length;
         sliderItems[currentIndex].classList.add("active");
-      }
+    }
 
     // Écouteurs d'événements pour les boutons de navigation
     sliderNavPrev.addEventListener("click", showPrevSlide);
